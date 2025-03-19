ccb417731438374d2b93bc38e1bbb1c3c5f4f35c
Matthis Gördel <matthis@posteo.de>
2025-03-19 16:42:51 +0100

    use unwrap in tests

--- network/tests/foo.rs
+++ network/tests/foo.rs
@@ -14,28 +14,24 @@ fn make_rt() -> Runtime {
 }
 
 #[test]
-fn start_and_stop_sender() -> Result<(), String> {
+fn start_and_stop_sender() {
     // TODO whyyyy stuck without subscriber?!
     tracing_subscriber::fmt().init();
 
     let service = sender::NetworkService::start(make_rt());
 
-    service.register_channel(String::from("127.0.0.1:0"), String::from("bar")).map_err(|e| e.to_string())?;
-    service.shutdown().map_err(|e| e.to_string())?;
-
-    Ok(())
+    service.register_channel(String::from("127.0.0.1:0"), String::from("bar")).unwrap();
+    service.shutdown().unwrap();
 }
 
 #[test]
-fn start_and_stop_receiver() -> Result<(), String> {
+fn start_and_stop_receiver() {
     tracing_subscriber::fmt().init();
 
     let service = receiver::NetworkService::start(make_rt(), String::from("127.0.0.1:0"));
 
-    service.register_channel(String::from("bar")).map_err(|e| e.to_string())?;
-    service.shutdown().map_err(|e| e.to_string())?;
-
-    Ok(())
+    service.register_channel(String::from("bar")).unwrap();
+    service.shutdown().unwrap();
 }
 
 fn make_tb(seq_no: u64) -> TupleBuffer {
@@ -52,7 +48,7 @@ fn make_tb(seq_no: u64) -> TupleBuffer {
 }
 
 #[test]
-fn send_and_receive() -> Result<(), String> {
+fn send_and_receive() {
     tracing_subscriber::fmt().init();
 
     let conn = String::from("127.0.0.1:13254");
@@ -61,27 +57,25 @@ fn send_and_receive() -> Result<(), String> {
     let rceivr = receiver::NetworkService::start(make_rt(), conn.clone());
     let sender = sender::NetworkService::start(make_rt());
 
-    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).map_err(|e| e.to_string())?;
+    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).unwrap();
 
     let t = make_tb(0);
     let msg = ChannelControlMessage::Data(t.clone());
 
-    executor::block_on(sender_chan.send(msg)).map_err(|e| e.to_string())?;
+    executor::block_on(sender_chan.send(msg)).unwrap();
 
 
-    let rceivr_chan = rceivr.register_channel(chan.clone()).map_err(|e| e.to_string())?;
-    let t1 = rceivr_chan.recv_blocking().map_err(|e| e.to_string())?;
+    let rceivr_chan = rceivr.register_channel(chan.clone()).unwrap();
+    let t1 = rceivr_chan.recv_blocking().unwrap();
 
     assert_eq!(t, t1);
 
-    sender.shutdown().map_err(|e| e.to_string())?;
-    rceivr.shutdown().map_err(|e| e.to_string())?;
-
-    Ok(())
+    sender.shutdown().unwrap();
+    rceivr.shutdown().unwrap();
 }
 
 #[test]
-fn send_and_receive_1000() -> Result<(), String> {
+fn send_and_receive_1000() {
     tracing_subscriber::fmt().init();
 
     let conn = String::from("127.0.0.1:13254");
@@ -90,8 +84,8 @@ fn send_and_receive_1000() -> Result<(), String> {
     let rceivr = receiver::NetworkService::start(make_rt(), conn.clone());
     let sender = sender::NetworkService::start(make_rt());
 
-    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).map_err(|e| e.to_string())?;
-    let rceivr_chan = rceivr.register_channel(chan.clone()).map_err(|e| e.to_string())?;
+    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).unwrap();
+    let rceivr_chan = rceivr.register_channel(chan.clone()).unwrap();
 
     let num_send_recvs = 1000;
 
@@ -102,23 +96,21 @@ fn send_and_receive_1000() -> Result<(), String> {
         send_tbs.push(t.clone());
         let msg = ChannelControlMessage::Data(t.clone());
 
-        executor::block_on(sender_chan.send(msg)).map_err(|e| e.to_string())?;
+        executor::block_on(sender_chan.send(msg)).unwrap();
     }
 
 
     for i in 0..num_send_recvs {
-        let t = rceivr_chan.recv_blocking().map_err(|e| e.to_string())?;
+        let t = rceivr_chan.recv_blocking().unwrap();
         assert_eq!(send_tbs[i as usize], t);
     }
 
-    sender.shutdown().map_err(|e| e.to_string())?;
-    rceivr.shutdown().map_err(|e| e.to_string())?;
-
-    Ok(())
+    sender.shutdown().unwrap();
+    rceivr.shutdown().unwrap();
 }
 
 #[test]
-fn send_recv_restart_receiver() -> Result<(), String> {
+fn send_recv_restart_receiver() {
     tracing_subscriber::fmt().init();
 
     let conn = String::from("127.0.0.1:13254");
@@ -127,15 +119,15 @@ fn send_recv_restart_receiver() -> Result<(), String> {
     let rceivr = receiver::NetworkService::start(make_rt(), conn.clone());
     let sender = sender::NetworkService::start(make_rt());
 
-    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).map_err(|e| e.to_string())?;
+    let sender_chan = sender.register_channel(conn.clone(), chan.clone()).unwrap();
 
     let t = make_tb(1);
     let msg = ChannelControlMessage::Data(t.clone());
 
-    executor::block_on(sender_chan.send(msg)).map_err(|e| e.to_string())?;
+    executor::block_on(sender_chan.send(msg)).unwrap();
 
-    let rceivr_chan = rceivr.register_channel(chan.clone()).map_err(|e| e.to_string())?;
-    let t1 = rceivr_chan.recv_blocking().map_err(|e| e.to_string())?;
+    let rceivr_chan = rceivr.register_channel(chan.clone()).unwrap();
+    let t1 = rceivr_chan.recv_blocking().unwrap();
     assert_eq!(t, t1);
 
     std::mem::drop(rceivr);
@@ -145,13 +137,11 @@ fn send_recv_restart_receiver() -> Result<(), String> {
     let t = make_tb(2);
     let msg = ChannelControlMessage::Data(t.clone());
 
-    executor::block_on(sender_chan.send(msg)).map_err(|e| e.to_string())?;
+    executor::block_on(sender_chan.send(msg)).unwrap();
 
-    let rceivr_chan = rceivr.register_channel(chan.clone()).map_err(|e| e.to_string())?;
-    let t1 = rceivr_chan.recv_blocking().map_err(|e| e.to_string())?;
+    let rceivr_chan = rceivr.register_channel(chan.clone()).unwrap();
+    let t1 = rceivr_chan.recv_blocking().unwrap();
     assert_eq!(t, t1);
 
-    sender.shutdown().map_err(|e| e.to_string())?;
-
-    Ok(())
+    sender.shutdown().unwrap();
 }
